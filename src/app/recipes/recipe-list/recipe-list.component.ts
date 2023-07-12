9d1d697b6710274b6d17a3f2d600efb33ef34327
Anca Irom <anca.hiciu@gmail.com>
2023-07-12 17:50:51 +0300

    Display a list of recipes and create recipe detail html

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes : Recipe[]=[
+    new Recipe("Recipe1", "Tzuchini recipe", "https://theabsolutefoodie.com/wp-content/uploads/2022/07/indian-zucchini-recipes.jpg"),
+    new Recipe("Recipe2", "Tartar recipe", "https://www.inspiredtaste.net/wp-content/uploads/2019/02/Homemade-Tartar-Sauce-Recipe-1200.jpg"),
+  ];
 
 }
